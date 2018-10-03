32ba56eae8bf1e275cdd063679ae580aeb6be12f
Gustavo <gustavo.reyes1@upr.edu>
2018-10-02 21:19:02 -0400

    ALU, forgot to upload changes

--- ALU/ALU.v
+++ ALU/ALU.v
@@ -1,4 +1,4 @@
-module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, operation, a, b); 
+module ALU(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, operation, a, b); 
     input [31:0] a; 
     input [31:0] b; 
     input [3:0] operation;
@@ -12,8 +12,8 @@ module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, opera
 
     integer index;
     integer counter = 0;
-    integer val = 0;
-    reg [31:0 ]temp1;
+    integer var = 0;
+    reg [31:0 ]tempVar;
 
     always@(a or b or operation) 
         begin 
@@ -61,9 +61,9 @@ module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, opera
 
                 4'b0111: // subtraction
                 begin
-                    temp1 = (~b  + 1'b1);
-                    Result = $signed(a) + $signed(temp1);
-                    overFlowFlag = (a[31]!= temp1[31])? 0 : (temp1[31] == Result[31]) ? 0: 1 ;
+                    tempVar = (~b  + 1'b1);
+                    Result = $signed(a) + $signed(tempVar);
+                    overFlowFlag = (a[31]!= tempVar[31])? 0 : (tempVar[31] == Result[31]) ? 0: 1 ;
                     negativeFlag = (Result[31] == 1)? 1 : 0 ; 
                     zeroFlag = (Result == 0) ? 1 : 0;
                 end
@@ -89,8 +89,7 @@ module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, opera
                     Result = a >> b;
                 end
 
-                4'b1100: // SLT: if Result of a < b then return 1 else 0.
-                        // SIGNED OPERATION. In verilog 0 == true, 1 == false
+                4'b1100: // SLT 0 == true 1 == false because of verilog
                 begin
                     if ((a < b) == 0) begin
                         Result = 1;
@@ -99,8 +98,7 @@ module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, opera
                     end
                 end
 
-                4'b1101: // SLTU: if Result of a < b then return 1 else 0.
-                        // UNSIGNED OPERATION. In verilog 0 == true, 1 == false
+                4'b1101: // SLTU
                 begin
                     if ((a < b) == 0) begin
                         Result = 1;
@@ -109,14 +107,14 @@ module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, opera
                     end
                 end
 
-                4'b1110: // CLO: count of leading ones in a, return in "Result" the number of leading ones;
+                4'b1110: // CLO
                 begin
                     for(index = 31; index >= 0; index = index-1) begin  
                         if(a[index] == 1'b0) begin
-                            val = 1;
+                            var = 1;
                             index = -1;
                         end 
-                        if(val == 0) begin
+                        if(var == 0) begin
                             counter = counter + 1;
                         end
                     end
@@ -124,14 +122,14 @@ module ALU_32_bit(Result, carryFlag, negativeFlag, overFlowFlag, zeroFlag, opera
                 end
                 
 
-                4'b1111: // CLZ: count of leading zeros in a, return in "Result" the number of leading zeros.
+                4'b1111: // CLZ
                 begin
                     for(index = 31; index >= 0; index = index-1) begin  
                         if(a[index] == 1'b1) begin
-                            val = 1;
+                            var = 1;
                             index = -1;
                         end 
-                        if(val == 0) begin
+                        if(var == 0) begin
                             counter = counter + 1;
                         end
                     end
--- ALU/ALU_Test.v
+++ ALU/ALU_Test.v
@@ -1,231 +1,86 @@
-// //Author: Roberto Y.  Guzmán Ortíz
-
-// module ALU32bit(output reg [31:0] result, output reg C_flag, N_flag, Z_flag, V_flag, input [4:0]opCode, input [31:0] a, b);
-//   reg [31:0 ]temp1;
-//   integer index;
-//   integer counter = 0;
-//   integer val = 0;
-
-//   always @(opCode,a, b)
-//     case(opCode)
-//       5'd0: // AND 
-//         begin
-//           assign result = a & b;
-//         end
-
-//       5'd1: // OR
-//         begin
-//           assign result = a | b;
-//         end
-
-//       5'd2: // XOR
-//         begin
-//           assign result = a ^ b;
-//         end
-
-//       5'd3: // NOR 
-//         begin
-//           assign result = ~(a | b);
-//         end
-
-//       5'd4: // SLL
-//         begin
-//           assign {C_flag,result} = a << 1;
-//         end
-
-//       5'd5: // SLLV
-//         begin
-//           assign {C_flag, result} = a << b;
-//         end
-
-//       5'd6: // SRL
-//         begin
-//           assign {C_flag, result} = a >> 1;
-//         end
-
-//       5'd7: // SRLV
-//         begin
-//           assign {C_flag, result} = a >> b;
-//         end
-
-//       5'd8: // ADDU: sum unsigned
-//         begin
-//           assign {C_flag,result} = a + b;
-//         end
-
-//       5'd9: // SUBU: sub unsigned
-//         begin 
-//           assign {C_flag,result} = a - b;      
-//         end
-
-//       5'd10: // ADD: addition signed
-//         begin
-//           assign result = $signed(a) + $signed(b);
-//           assign V_flag = (a[31] != b[31])? 0 : (b[31] == result[31]) ? 0: 1 ;
-//           assign N_flag = (result[31] == 1)? 1 : 0 ; 
-//           assign Z_flag = (result == 0) ? 1 : 0;
-//         end
-
-//       5'd11: // SUB: subtract signed
-//         begin
-//           temp1 = (~b  + 1'b1);
-//           assign result = $signed(a) + $signed(temp1);
-//           assign V_flag = (a[31]!= temp1[31])? 0 : (temp1[31] == result[31]) ? 0: 1 ;
-//           assign N_flag = (result[31] == 1)? 1 : 0 ; 
-//           assign Z_flag = (result == 0) ? 1 : 0;
-//         end
-//       5'd12:
-//         begin
-//           // SLT: if result of a < b then return 1 else 0.
-//           assign result = ($signed(a) < $signed(b)) ? 1 : 0;
-//         end
-
-//       5'd13:
-//         begin
-//           // SLTU: if result of a < b then return 1 else 0.
-//           assign result = (a < b) ? 1 : 0;
-//         end
-
-//       5'd14:
-//          begin
-//           // CLO: count of leading ones in a, return in "result" the number of leading ones;
-//           counter = 0;
-//           index = 0;
-//           while (a[index] == 1'b1) begin
-//             counter = counter + 1;
-//             index = index + 1;
-//           end
-//           assign result = counter;
-//         end
-
-//       5'd15:
-//         begin
-//           // CLZ: count of leading zeros in a, return in "result" the number of leading zeros.
-//           counter = 0;
-//           index = 0;
-//           while (a[index] == 1'b0) begin
-//             counter = counter + 1;
-//             index = index + 1;
-//           end
-//           assign result = counter;
-//         end
-//     endcase
-// endmodule
-
-//MAIN MODULE
 module main ();
 
-  /////////////////////VARIABLE DECLARATION/////////////////////
-  
-  //inputs
-  reg [31:0] r1, r2;
-  reg [3:0] code;
+  reg [31:0] a, b;
+  reg [3:0] op;
 
-  //outputs
-  wire  [31:0] result;
+  wire  [31:0] ans;
   wire carry;
-  wire negative;
+  wire neg;
   wire zero;
-  wire overflow;
+  wire over;
+  reg [8*6:1] operation;
+  parameter timer = 1900;
 
-  //operation name
-  reg [8*6:1] opName;
+  ALU test(ans, carry, neg, zero, over, op, a, b); //ALU
 
-  // simulation time
-  parameter sim_time = 1900;
+  initial #timer $finish;
 
-  // ALU module initialization
-  ALU_32_bit testALU(result, carry, negative, zero, overflow, code, r1, r2);
-
-  initial #sim_time $finish;
-  /////////////////////VARIABLE DECLARATION/////////////////////
-
-
-
-  /////////////////////SET VALUES TO TEST/////////////////////
   initial begin
-    opName = "AND"; r1 = 32'h0; r2 = 32'd0; code = 5'd0;
-    #10 opName = "AND"; r1 = 32'h00000000; r2 = 32'hFFFFFFFF; 
-    #10 opName = "AND"; r1 = 32'hFFFFFFFF; r2 = 32'h00000000;
-    #10 opName = "AND"; r1 = 32'hFFFFFFFF; r2 = 32'hFFFFFFFF;
-    #10 opName = "AND"; r1 = 32'hAAAAAAAA; r2 = 32'hFFFFFFFF;
-    #10 opName = "AND"; r1 = 32'hAAAAAAAA; r2 = 32'h55555555;
-    #10 opName = "AND"; r1 = 32'h12345678; r2 = 32'h9ABCDEF0;
+    operation = "AND"; a = 32'h0; b = 32'd0; op = 5'd0;
+    #10 operation = "AND"; a = 32'h00000000; b = 32'hFFFFFFFF; 
+    #10 operation = "AND"; a = 32'hFFFFFFFF; b = 32'h00000000;
+    #10 operation = "AND"; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF;
     #10 $display("");
 
-    opName = "OR"; r1 = 32'h0; r2 = 32'd0; code = 5'd1;
-    #10 opName = "OR"; r1 = 32'h00000000; r2 = 32'hFFFFFFFF; 
-    #10 opName = "OR"; r1 = 32'hFFFFFFFF; r2 = 32'h00000000;
-    #10 opName = "OR"; r1 = 32'hFFFFFFFF; r2 = 32'hFFFFFFFF;
-    #10 opName = "OR"; r1 = 32'hAAAAAAAA; r2 = 32'hFFFFFFFF;
-    #10 opName = "OR"; r1 = 32'hAAAAAAAA; r2 = 32'h55555555;
-    #10 opName = "OR"; r1 = 32'h12345678; r2 = 32'h9ABCDEF0;
+    operation = "OR"; a = 32'h0; b = 32'd0; op = 5'd1;
+    #10 operation = "OR"; a = 32'h00000000; b = 32'hFFFFFFFF; 
+    #10 operation = "OR"; a = 32'hFFFFFFFF; b = 32'h00000000;
+    #10 operation = "OR"; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF;
     #10 $display("");
 
-    opName = "XOR"; r1 = 32'h0; r2 = 32'd0; code = 5'd2;
-    #10 opName = "XOR"; r1 = 32'h00000000; r2 = 32'hFFFFFFFF; 
-    #10 opName = "XOR"; r1 = 32'hFFFFFFFF; r2 = 32'h00000000;
-    #10 opName = "XOR"; r1 = 32'hFFFFFFFF; r2 = 32'hFFFFFFFF;
-    #10 opName = "XOR"; r1 = 32'hAAAAAAAA; r2 = 32'hFFFFFFFF;
-    #10 opName = "XOR"; r1 = 32'hAAAAAAAA; r2 = 32'h55555555;
-    #10 opName = "XOR"; r1 = 32'h12345678; r2 = 32'h9ABCDEF0;
+    operation = "XOR"; a = 32'h0; b = 32'd0; op = 5'd2;
+    #10 operation = "XOR"; a = 32'h00000000; b = 32'hFFFFFFFF; 
+    #10 operation = "XOR"; a = 32'hFFFFFFFF; b = 32'h00000000;
+    #10 operation = "XOR"; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF;
     #10 $display("");
 
-    opName = "NOR"; r1 = 32'h0; r2 = 32'd0; code = 5'd3;
-    #10 opName = "XOR"; r1 = 32'h00000000; r2 = 32'hFFFFFFFF; 
-    #10 opName = "XOR"; r1 = 32'hFFFFFFFF; r2 = 32'h00000000;
-    #10 opName = "XOR"; r1 = 32'hFFFFFFFF; r2 = 32'hFFFFFFFF;
-    #10 opName = "XOR"; r1 = 32'hAAAAAAAA; r2 = 32'hFFFFFFFF;
-    #10 opName = "XOR"; r1 = 32'hAAAAAAAA; r2 = 32'h55555555;
-    #10 opName = "XOR"; r1 = 32'h12345678; r2 = 32'h9ABCDEF0;
+    operation = "NOR"; a = 32'h0; b = 32'd0; op = 5'd3;
+    #10 operation = "NOR"; a = 32'h00000000; b = 32'hFFFFFFFF; 
+    #10 operation = "NOR"; a = 32'hFFFFFFFF; b = 32'h00000000;
+    #10 operation = "NOR"; a = 32'hFFFFFFFF; b = 32'hFFFFFFFF;
     #10 $display("");
     
-    opName = "SLL"; r1 = 32'h0; r2 = 32'd0; code = 5'd4;
+    operation = "ADDU"; a = 32'h7; b = 32'd3; op = 5'd4;
     #10 $display("");
 
-    opName = "SLLV"; r1 = 32'h0; r2 = 32'd0; code = 5'd5;
+    operation = "SUBU"; a = 32'h2; b = 32'd1; op = 5'd5;
+    #10 $display("");
+    
+    operation = "ADD"; a = 32'h9; b = 32'd1; op = 5'd6;
     #10 $display("");
 
-    opName = "SRL"; r1 = 32'h0; r2 = 32'd0; code = 5'd6;
+    operation = "SUB"; a = 32'h9; b = 32'd3; op = 5'd7;
     #10 $display("");
 
-    opName = "SRLV"; r1 = 32'h0; r2 = 32'd0; code = 5'd7;
+    operation = "SLL"; a = 32'h1; b = 32'd1; op = 5'd8;
     #10 $display("");
 
-    opName = "ADDU"; r1 = 32'h0; r2 = 32'd0; code = 5'd8;
+    operation = "SLLV"; a = 32'h1; b = 32'd1; op = 5'd9;
     #10 $display("");
 
-    opName = "SUBU"; r1 = 32'h0; r2 = 32'd0; code = 5'd9;
+    operation = "SRL"; a = 32'h1; b = 32'd1; op = 5'd10;
     #10 $display("");
 
-    opName = "ADD"; r1 = 32'h0; r2 = 32'd0; code = 5'd10;
+    operation = "SRLV"; a = 32'h1; b = 32'd2; op = 5'd11;
     #10 $display("");
 
-    opName = "SUB"; r1 = 32'h0; r2 = 32'd0; code = 5'd11;
+    operation = "SLT"; a = 32'h1; b = 32'd1; op = 5'd12;
     #10 $display("");
 
-    opName = "SLT"; r1 = 32'h0; r2 = 32'd0; code = 5'd12;
+    operation = "SLTU"; a = 32'h1; b = 32'd1; op = 5'd13;
     #10 $display("");
 
-    opName = "SLTU"; r1 = 32'h0; r2 = 32'd0; code = 5'd13;
+    operation = "CLO"; a = 32'h1; b = 32'd1; op = 5'd14;
     #10 $display("");
 
-    opName = "CLO"; r1 = 32'h0; r2 = 32'd0; code = 5'd14;
-    #10 $display("");
-
-    opName = "CLZ"; r1 = 32'h0; r2 = 32'd0; code = 5'd15;
+    operation = "CLZ"; a = 32'h1; b = 32'd1; op = 5'd15;
     #10 $display("");
 
   end
-  /////////////////////SET VALUES TO TEST/////////////////////
-
-
-  /////////////////////DO THE THING/////////////////////
 
   initial begin
-    $display ("time         opName          r1          r2          carry         code          FLAGS(NZCV)                    out-bin                       out-decimal");
-    $monitor ("%4d        %s         %h    %h        %b           %b            %b%b%b%b            %b          %d", $time, opName, r1, r2, carry, code, negative, zero, carry, overflow, result, result);
+    $display ("time         operation          a          b          carry        opCode         N-Z-C-V                     binary                        decimal");
+    $monitor ("%4d        %s         %h    %h        %b           %b            %b%b%b%b            %b          %d", $time, operation, a, b, carry, op, neg, zero, carry, over, ans, ans);
   end
- /////////////////////DO THE THING/////////////////////
 
 endmodule
