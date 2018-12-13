109ba8c2167aae70dffa97dbefae4713cc4a36de
Your Name <you@example.com>
2018-12-13 01:08:33 -0400

    Initialize test

--- cleanup/RegisterFile.v
+++ cleanup/RegisterFile.v
@@ -13,7 +13,39 @@ module RegisterFile(A_Address, B_Address, C_Address, C_Data, Write, Clk, A_Data,
 	
 	
 	initial begin
-		Registers[0] <= 32'd0;	
+		Registers[0] <= 32'd0;
+		Registers[1] <= 32'd0;
+		Registers[2] <= 32'd0;
+		Registers[3] <= 32'd0;
+		Registers[4] <= 32'd0;
+		Registers[5] <= 32'd0;
+		Registers[6] <= 32'd0;
+		Registers[7] <= 32'd0;
+		Registers[8] <= 32'd0;
+		Registers[9] <= 32'd0;
+		Registers[10] <= 32'd0;
+		Registers[11] <= 32'd0;
+		Registers[12] <= 32'd0;
+		Registers[13] <= 32'd0;
+		Registers[14] <= 32'd0;
+		Registers[15] <= 32'd0;
+		Registers[16] <= 32'd0;
+		Registers[17] <= 32'd0;
+		Registers[18] <= 32'd0;
+		Registers[19] <= 32'd0;
+		Registers[20] <= 32'd0;
+		Registers[21] <= 32'd0;
+		Registers[22] <= 32'd0;
+		Registers[23] <= 32'd0;
+		Registers[24] <= 32'd0;
+		Registers[25] <= 32'd0;
+		Registers[26] <= 32'd0;
+		Registers[27] <= 32'd0;
+		Registers[28] <= 32'd0;
+		Registers[29] <= 32'd0;
+		Registers[30] <= 32'd0;
+		Registers[31] <= 32'd0;
+			
 	end
 	
 	always @(posedge Clk)
