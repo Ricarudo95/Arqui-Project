43619b9afc89d9925cc3092937e3efe7b26f3905
Gustavo Reyes <gustavo.reyes1@upr.edu>
2018-12-14 00:35:30 -0400

    dsdsds

--- cleanup/testers/CPUTest1.v
+++ cleanup/testers/CPUTest1.v
@@ -61,20 +61,15 @@ module CPUTester1();
             for(index = 1; index <= 130; index = index+1) begin  
                 clk =0; #5 clk = 1; #5;
 
-                
-                /*$display("\nProgram Counter: %d", CPU_Test1.Program_Counter.PCResult );
-                $display("\nCurrent Instruction: %b", CPU_Test1.Instruction_Memory.Instruction );
-                $display("\nOperation Code: %b", CPU_Test1.Control_Unit.opcode );
-                $display("\nRegister S Address: %d", CPU_Test1.Register_File.A_Address );
-                $display("\nRegister T Adresss: %d", CPU_Test1.Register_File.B_Address );
-                $display("\nOffset: %d\n\n", CPU_Test1.signExt.ins );
-                
                 $fwrite(f,"\nProgram Counter: %d", CPU_Test1.Program_Counter.PCResult );
                 $fwrite(f,"\nCurrent Instruction: %b", CPU_Test1.Instruction_Memory.Instruction );
                 $fwrite(f,"\nOperation Code: %b", CPU_Test1.Control_Unit.opcode );
                 $fwrite(f,"\nRegister S Address: %d", CPU_Test1.Register_File.A_Address );
-                $fwrite(f,"\nRegister T Adresss: %d", CPU_Test1.Register_File.B_Address );
-                $fwrite(f,"\nOffset: %d\n\n", CPU_Test1.signExt.ins );*/
+                $fwrite(f,"\nRegister T Address: %d", CPU_Test1.Register_File.B_Address );
+                $fwrite(f,"\nOffset: %d\n\n", CPU_Test1.signExt.ins );
+
+                $fwrite(f,"\nMAR: %b", CPU_Test1.MAR.Result );
+                $fwrite(f,"\nMemory Address: %b", CPU_Test1.Instruction_Memory.Mem );
                 
                
             end
