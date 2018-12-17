0695a637d57c64ce654b0e009227afc0bd378b4f
GarcGustavo <gustavo.reyes1@upr.edu>
2018-12-16 22:59:47 -0400

    other testers

--- cleanup/CPU.v
+++ cleanup/CPU.v
@@ -123,7 +123,7 @@ AND simpleAND(andOut, branch, zFlag);
 
 endmodule 
 
-/*module mipsCPUData2(clk,reset);  
+module mipsCPUData2(clk,reset);  
 
     input  clk;
     input reset;
@@ -142,7 +142,7 @@ endmodule
     wire [31:0] instruction;
     
     //Register
-    wire [31:0] aluA;
+    wire [31:0] marInput;
     wire [31:0] regOutA; 
     wire [31:0] regOutB;
     wire [4:0] regMuxOut; 
@@ -168,8 +168,7 @@ endmodule
     wire [31:0] shftLeftOut;
     wire [31:0] branchAddOut;
     wire [31:0] branchSelect;
-    wire andOut; 
-  
+    wire andOut;
 
 
 ////////// STATE FLAGS ////////////
@@ -198,10 +197,10 @@ endmodule
 /////////////////////////COMPONENTS////////////////////////////////////
 
 //MAR and MDR
-register MAR(memAdress, aluOut, marLoad);
-register MDR(mdrData, mdrIn, mdrLoad);
-register NPC(next,jumpMuxOut,npcLoad);
-register IR(instruction, memData, irLoad);
+register MAR(memAdress, marInput, marLoad,clk);
+register MDR(mdrData, mdrIn, mdrLoad,clk);
+register NPC(next, jumpMuxOut,npcLoad,clk);
+register IR(instruction, memData, irLoad,clk);
 
 //Program Counter
 ProgramCounter Program_Counter(next, pcOut, reset, clk, pcLoad);
@@ -212,7 +211,7 @@ control Control_Unit(clk, reset, MOC, instruction[31:26], unSign, memEnable, irL
 //Mux Connections
 //muxA BasicMux(muxAout, HILO, instruction[25:21], LO, HI); //not used
 
-mux32 pcMux(aluA, pcSelect, regOutA, pcOut );
+mux32 pcMux(marInput, pcSelect, aluOut, pcOut);
 mux6 funcMux(func, immediate, instruction[5:0], aluCode);
 mux4 Register_Mux(regMuxOut, rfSource, instruction[20:16], instruction[15:11]); //present
 //mux32 ALU_Mux(aluB, aluSource, regOutB, signExtOut);
@@ -220,16 +219,16 @@ mux4inputs ALU_Mux(aluB, aluSource, regOutB, signExtOut, mdrData, 32'd0);
 mux32 mdrMux(mdrIn, mdrSource, memData, aluOut);
 mux32 Branch_Mux(branchSelect, andOut, pcAdd4, branchAddOut);
 mux32 Jump_Mux(jumpMuxOut, jump, branchSelect, {pcAdd4[31:28], shftLeft28Out});
- 
+
 
 //Register File
 RegisterFile Register_File(instruction[25:21], instruction[20:16], regMuxOut, mdrIn, regWrite, clk, regOutA, regOutB );
 
 //ALU Modules
-ALU alu(aluOut, zFlag, func, aluA, aluB);
+ALU alu(aluOut, zFlag, func, regOutA, aluB);
 
 //RAM Module
-MemoryTest2 Memory(memAdress, memData, mdrData, rw, byte, MOC, memEnable);
+MemoryTest1 Memory(memAdress, memData, mdrData, rw, byte, MOC, memEnable);
 
 
 //Util Modules
@@ -237,11 +236,17 @@ signExtender signExt(signExtOut, instruction[15:0], unSign);
 shftLeft28 shftJump(shftLeft28Out, instruction[25:0]);
 shftLeft shftLeft(shftLeftOut,signExtOut);
 addplus4 addFour(pcAdd4, pcOut);
-adder adder(branchAddOut, pcAdd4, shftLeftOut);
-AND simpleAND(andOut, branch, zFlag );
+adder adder(branchAddOut, pcOut, shftLeftOut);
+AND simpleAND(andOut, branch, zFlag);
 
 
-endmodule  
+////////// MONITOR ////////////
+// initial begin
+// $dumpvars(0,instruction);
+// $monitor("Intructions [B]", instruction);
+// end
+
+endmodule 
 
 module mipsCPUData3(clk,reset);  
 
@@ -262,7 +267,7 @@ module mipsCPUData3(clk,reset);
     wire [31:0] instruction;
     
     //Register
-    wire [31:0] aluA;
+    wire [31:0] marInput;
     wire [31:0] regOutA; 
     wire [31:0] regOutB;
     wire [4:0] regMuxOut; 
@@ -288,8 +293,7 @@ module mipsCPUData3(clk,reset);
     wire [31:0] shftLeftOut;
     wire [31:0] branchAddOut;
     wire [31:0] branchSelect;
-    wire andOut; 
-  
+    wire andOut;
 
 
 ////////// STATE FLAGS ////////////
@@ -318,10 +322,10 @@ module mipsCPUData3(clk,reset);
 /////////////////////////COMPONENTS////////////////////////////////////
 
 //MAR and MDR
-register MAR(memAdress, aluOut, marLoad);
-register MDR(mdrData, mdrIn, mdrLoad);
-register NPC(next,jumpMuxOut,npcLoad);
-register IR(instruction, memData, irLoad);
+register MAR(memAdress, marInput, marLoad,clk);
+register MDR(mdrData, mdrIn, mdrLoad,clk);
+register NPC(next, jumpMuxOut,npcLoad,clk);
+register IR(instruction, memData, irLoad,clk);
 
 //Program Counter
 ProgramCounter Program_Counter(next, pcOut, reset, clk, pcLoad);
@@ -332,7 +336,7 @@ control Control_Unit(clk, reset, MOC, instruction[31:26], unSign, memEnable, irL
 //Mux Connections
 //muxA BasicMux(muxAout, HILO, instruction[25:21], LO, HI); //not used
 
-mux32 pcMux(aluA, pcSelect, regOutA, pcOut );
+mux32 pcMux(marInput, pcSelect, aluOut, pcOut);
 mux6 funcMux(func, immediate, instruction[5:0], aluCode);
 mux4 Register_Mux(regMuxOut, rfSource, instruction[20:16], instruction[15:11]); //present
 //mux32 ALU_Mux(aluB, aluSource, regOutB, signExtOut);
@@ -340,16 +344,16 @@ mux4inputs ALU_Mux(aluB, aluSource, regOutB, signExtOut, mdrData, 32'd0);
 mux32 mdrMux(mdrIn, mdrSource, memData, aluOut);
 mux32 Branch_Mux(branchSelect, andOut, pcAdd4, branchAddOut);
 mux32 Jump_Mux(jumpMuxOut, jump, branchSelect, {pcAdd4[31:28], shftLeft28Out});
- 
+
 
 //Register File
 RegisterFile Register_File(instruction[25:21], instruction[20:16], regMuxOut, mdrIn, regWrite, clk, regOutA, regOutB );
 
 //ALU Modules
-ALU alu(aluOut, zFlag, func, aluA, aluB);
+ALU alu(aluOut, zFlag, func, regOutA, aluB);
 
 //RAM Module
-MemoryTest3 Memory(memAdress, memData, mdrData, rw, byte, MOC, memEnable);
+MemoryTest1 Memory(memAdress, memData, mdrData, rw, byte, MOC, memEnable);
 
 
 //Util Modules
@@ -357,114 +361,8 @@ signExtender signExt(signExtOut, instruction[15:0], unSign);
 shftLeft28 shftJump(shftLeft28Out, instruction[25:0]);
 shftLeft shftLeft(shftLeftOut,signExtOut);
 addplus4 addFour(pcAdd4, pcOut);
-adder adder(branchAddOut, pcAdd4, shftLeftOut);
-AND simpleAND(andOut, branch, zFlag );
-
-
-endmodule  
-
-
-/*module mipsCPUData2(clk,reset);  
-
-    input  clk;
-    input reset;
-  
-
-////////////// CIRCUIT CONNECTIONS ///////////////
-
-    //Program Counter And Intructions
-    wire[31:0] next;
-    wire [31:0] pcOut; 
-    wire [31:0] instruction;
-    
-    //Register
-    wire [31:0] aluA; 
-    wire [31:0] regOutB;
-    wire [4:0] regMuxOut; 
-    //NEW
-    wire [31:0] mdrData;
-    wire [31:0] PC;
-    wire [31:0] MAR;
-    wire [31:0] NPC;
-    wire [31:0] IR;
-
-    //ALU
-    wire [31:0] aluB; 
-    wire [31:0] aluOut;
-    wire zFlag;
-
-    //Ram
-    wire [31:0] memData;
-    wire [31:0] mdrIn;
-
-    //Jump
-    wire [31:0] pcAdd4; 
-    wire [27:0] shftLeft28Out;
-    wire [31:0] jumpMuxOut;
-
-
-    //Branch
-    wire [31:0] signExtOut;
-    wire [31:0] shftLeftOut;
-    wire [31:0] branchAddOut;
-    wire [31:0] branchSelect; 
-    wire andOut; 
-  
-
-
-////////// STATE FLAGS ////////////
-    wire rfSource;
-    wire reg_write;
-    wire aluSource;
-    wire memRead;
-    wire memWrite; //RW
-    wire mem_to_reg;
-    wire jump;  
-    wire branch; 
-    wire unSign; 
-    wire [2:0] aluCode ;
-
-
-
-/////////////////////////COMPONENTS////////////////////////////////////
-
-//Program Counter
-ProgramCounter Program_Counter(next, pcOut, reset, clk);
-
-//reg [511:0] PC = 512'd0;
-//Intruction Memory
-instructMemTest2 Instruction_Memory(instruction, clk, pcOut);
-
-//Control Unit
-control Control_Unit(clk, instruction[31:26], reset, rfSource, reg_write, aluSource, memRead, memWrite, mem_to_reg,jump, branch, unSign, aluCode);
-
-//Mux Connections
-//muxA BasicMux(muxAout, HILO, instruction[25:21], LO, HI); //not used
-
-
-mux4 Register_Mux(regMuxOut, rfSource, instruction[20:16], instruction[15:11]); //present
-mux32 ALU_Mux(aluB, aluSource, regOutB, signExtOut);
-mux32 RAM_Mux(mdrIn, mem_to_reg, memData, aluOut);
-mux32 Branch_Mux(branchSelect, andOut, pcAdd4, branchAddOut);
-mux32 Jump_Mux(next, jump, branchSelect, {pcAdd4[31:28], shftLeft28Out}); 
-
-//Register File
-RegisterFile Register_File(instruction[25:21], instruction[20:16], regMuxOut, mdrIn, mem_to_reg, clk, aluA, regOutB );
-
-//ALU Modules
-ALU alu(aluOut, zFlag, instruction[5:0], aluA, aluB, aluCode);
-
-//RAM Module
-RAM ram(clk, aluOut, memData, regOutB,rw, mdrData);
-
-
-//Util Modules
-signExtender signExt(signExtOut, instruction[15:0], unSign);
-shftLeft28 shftJump(shftLeft28Out, instruction[25:0]);
-shftLeft shftLeft(shftLeftOut,signExtOut);
-addplus4 addFour(pcAdd4, pcOut);
-adder adder(branchAddOut, pcAdd4, shftLeftOut);
-AND simpleAND(andOut, branch, zFlag );
+adder adder(branchAddOut, pcOut, shftLeftOut);
+AND simpleAND(andOut, branch, zFlag);
 
 
 ////////// MONITOR ////////////
@@ -473,109 +371,4 @@ AND simpleAND(andOut, branch, zFlag );
 // $monitor("Intructions [B]", instruction);
 // end
 
-endmodule  */
-
-/*module mipsCPUData3(clk,reset);  
-
-    input  clk;
-    input reset;
-  
-
-////////////// CIRCUIT CONNECTIONS ///////////////
-
-    //Program Counter And Intructions
-    wire[31:0] next;
-    wire [31:0] pcOut; 
-    wire [31:0] instruction;
-    
-    //Register
-    wire [31:0] aluA; 
-    wire [31:0] regOutB;
-    wire [4:0] regMuxOut; 
-
-    //ALU
-    wire [31:0] aluB; 
-    wire [31:0] aluOut;
-    wire zFlag;
-
-    //Ram
-    wire [31:0] memData;
-    wire [31:0] mdrIn;
-
-    //Jump
-    wire [31:0] pcAdd4; 
-    wire [27:0] shftLeft28Out;
-    wire [31:0] jumpMuxOut;
-
-
-    //Branch
-    wire [31:0] signExtOut;
-    wire [31:0] shftLeftOut;
-    wire [31:0] branchAddOut;
-    wire [31:0] branchSelect; 
-    wire andOut; 
-  
-
-
-////////// STATE FLAGS ////////////
-    wire rfSource;
-    wire reg_write;
-    wire aluSource;
-    wire memRead;
-    wire memWrite; //RW
-    wire mem_to_reg;
-    wire jump;  
-    wire branch; 
-    wire unSign; 
-    wire [2:0] aluCode ;
-
-
-
-/////////////////////////COMPONENTS////////////////////////////////////
-
-//Program Counter
-ProgramCounter Program_Counter(next, pcOut, reset, clk);
-
-//reg [511:0] PC = 512'd0;
-//Intruction Memory
-instructMemTest3 Instruction_Memory(instruction, clk, pcOut);
-
-//Control Unit
-control Control_Unit(clk, instruction[31:26], reset, rfSource, reg_write, aluSource, memRead, memWrite, mem_to_reg,jump, branch, unSign, aluCode);
-
-//Mux Connections
-//muxA BasicMux(muxAout, HILO, instruction[25:21], LO, HI); //not used
-
-
-mux4 Register_Mux(regMuxOut, rfSource, instruction[20:16], instruction[15:11]); //present
-mux4inputs ALU_Mux(aluB, aluSource, regOutB, signExtOut, mdrData, 32'd0);
-mux32 RAM_Mux(mdrIn, mem_to_reg, memData, aluOut);
-mux32 Branch_Mux(branchSelect, andOut, pcAdd4, branchAddOut);
-mux32 Jump_Mux(next, jump, branchSelect, {pcAdd4[31:28], shftLeft28Out}); 
-
-//Register File
-RegisterFile Register_File(instruction[25:21], instruction[20:16], regMuxOut, mdrIn, mem_to_reg, clk, aluA, regOutB );
-
-//ALU Modules
-ALU alu(aluOut, zFlag, instruction[5:0], aluA, aluB, aluCode);
-
-//RAM Module
-RAM ram(clk, memRead, memWrite, aluOut, memData, regOutB);
-
-
-//Util Modules
-signExtender signExt(signExtOut, instruction[15:0], unSign);
-shftLeft28 shftJump(shftLeft28Out, instruction[25:0]);
-shftLeft shftLeft(shftLeftOut,signExtOut);
-addplus4 addFour(pcAdd4, pcOut);
-adder adder(branchAddOut, pcAdd4, shftLeftOut);
-AND simpleAND(andOut, branch, zFlag );
-
-
-////////// MONITOR ////////////
-// initial begin
-// $dumpvars(0,instruction);
-// $monitor("Intructions [B]", instruction);
-// end
-
-endmodule  */
+endmodule 
--- cleanup/testers/CPUTest2.v
+++ cleanup/testers/CPUTest2.v
@@ -60,31 +60,31 @@ module CPUTester2();
             f = $fopen("output/StateChangeTest2.txt","w");
 
             $fwrite(memoryFile,"\n----------Memory at Start Up---------------\n");
-            for(index = 0; index <= 199; index = index+4) begin
-            $fwrite(memoryFile,"\nMemory[%3d]: %8b  Memory[%3d]: %8b  Memory[%3d]: %8b  Memory[%3d]: %8b", index,CPU_Test1.Memory.Mem[index],index+1,CPU_Test1.Memory.Mem[index+1],index+2,CPU_Test1.Memory.Mem[index+2],index+3,CPU_Test1.Memory.Mem[index+3]);
+            for(index = 0; index <= 53; index = index+4) begin
+            $fwrite(memoryFile,"\nMemory[%2d]: %8b  Memory[%2d]: %8b  Memory[%2d]: %8b  Memory[%2d]: %8b", index,CPU_Test1.Memory.Mem[index],index+1,CPU_Test1.Memory.Mem[index+1],index+2,CPU_Test1.Memory.Mem[index+2],index+3,CPU_Test1.Memory.Mem[index+3]);
             end
 
-            for(index = 1; index <= 400; index = index+1) begin  
+            for(index = 1; index <= 600; index = index+1) begin  
                 clk =0; #5 clk = 1; #5;
 
                 $fwrite(f,"\n ---------CLOCK CYCLE: %d START-----------\n", index);
                 
                 $fwrite(f,"\n\nState: %d", CPU_Test1.Control_Unit.state );
-                $fwrite(f,"\nProgram Counter: %d", CPU_Test1.Program_Counter.PCResult );
+                //$fwrite(f,"\nProgram Counter: %d", CPU_Test1.Program_Counter.PCResult );
                 $fwrite(f,"\nCurrent Instruction: %b", CPU_Test1.Memory.output_destination );
-                $fwrite(f,"\nOperation Code: %b", CPU_Test1.Control_Unit.opCode );
-                $fwrite(f,"\nRegister S Address: %d", CPU_Test1.Register_File.A_Address );
-                $fwrite(f,"\nRegister T Address: %d", CPU_Test1.Register_File.B_Address );
+                //$fwrite(f,"\nOperation Code: %b", CPU_Test1.Control_Unit.opCode );
+                $fwrite(f,"\nRegister S Address: %d , %b", CPU_Test1.Register_File.A_Address, CPU_Test1.Register_File.A_Address );
+                $fwrite(f,"\nRegister T Address: %d , %b", CPU_Test1.Register_File.B_Address, CPU_Test1.Register_File.B_Address );
                 $fwrite(f,"\nOffset: %d\n\n", CPU_Test1.signExt.ins );
                 
-                $fwrite(f,"\nMAR: %b", CPU_Test1.MAR.result );
-                $fwrite(f,"\nMDR: %b", CPU_Test1.MDR.result );
-                $fwrite(f,"\nNPC: %b", CPU_Test1.NPC.result );
+                $fwrite(f,"\nMAR: %d", CPU_Test1.MAR.result );
+                //$fwrite(f,"\nMDR: %b", CPU_Test1.MDR.result );
+                //$fwrite(f,"\nNPC: %b", CPU_Test1.NPC.result );
                 $fwrite(f,"\nIR: %b", CPU_Test1.IR.result );
             end
             $fwrite(memoryFile,"\n----------Memory at End---------------\n");
-            for(index = 0; index <= 199; index = index+4) begin
-            $fwrite(memoryFile,"\nMemory[%3d]: %8b  Memory[%3d]: %8b  Memory[%3d]: %8b  Memory[%3d]: %8b", index,CPU_Test1.Memory.Mem[index],index+1,CPU_Test1.Memory.Mem[index+1],index+2,CPU_Test1.Memory.Mem[index+2],index+3,CPU_Test1.Memory.Mem[index+3]);
+            for(index = 0; index <= 53; index = index+4) begin
+            $fwrite(memoryFile,"\nMemory[%2d]: %8b Memory[%2d]: %8b Memory[%2d]: %8b Memory[%2d]: %8b", index,CPU_Test1.Memory.Mem[index],index+1,CPU_Test1.Memory.Mem[index+1],index+2,CPU_Test1.Memory.Mem[index+2],index+3,CPU_Test1.Memory.Mem[index+3]);
             end
             $fclose(f);
             $fclose(memoryFile);
